12cf373bea7ef249527cdecaf58b7037e96adb4f
Jesse Gould <jesse.gould@codeimmersives.com>
2019-12-02 18:59:58 -0500

    finished

--- main.js
+++ main.js
@@ -8,24 +8,42 @@ const makeDino = function(species, period, carnivore, extinct=false) {
 }
 
 const makeSingular = function(dino) {
-  if (!dino['species'].endsWith('us')) {
+  if (!dino.species.endsWith('us')) {
     return dino
   }
   else {
     return {
-    species: dino['species'].slice(0,-2),
-    period: dino['period'],
-    carnivore: dino['carnivore'],
-    extinct: dino['extinct'],
+    species: dino.species.slice(0,-2),
+    period: dino.period,
+    carnivore: dino.carnivore,
+    extinct: dino.extinct,
 }
   }
 }
 
-const truncateSpecies = function() {
+const truncateSpecies = function(dino) {
+  if (dino.species.length<=7){
+    return dino
+  }
+  else {
+     const truncDino = {
+    species: dino.species.slice(0,7) +'...',
+    period: dino.period,
+    carnivore: dino.carnivore,
+    extinct: dino.extinct,
+}
+return truncDino;
+}
 }
 
-const makeExtinct = function() {
-
+const makeExtinct = function(dino) {
+  const extinctDino = {
+    species: dino.species,
+    period: dino.period,
+    carnivore: dino.carnivore,
+    extinct: true,
+}
+return extinctDino;
 }
 
 module.exports = {
